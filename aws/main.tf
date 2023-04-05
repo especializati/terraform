13ee2fe70dea7ae251cf91df23970efc8e52d13a
Carlos Ferreira <carlos@especializati.com.br>
2023-04-05 17:47:49 -0300

    first commit

---
+++ aws/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  count         = 3
+  ami           = "ami-06e46074ae430fba6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-${count.index + 1}"
+  }
+
+  vpc_security_group_ids = ["sg-0fd0e521dd3c7ff5e"]
+  key_name = "terraform-key"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH"
+
+  ingress {
+    description      = "SSH access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_key_pair" "key-deployer" {
+  key_name = "terraform-key"
+  public_key = "${file("../ssh/terraform.pub")}"
+}
