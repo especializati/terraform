a2b1cf31244a511e3eeb6187206851b92a03f877
Carlos Ferreira <carlos@especializati.com.br>
2023-04-05 17:48:01 -0300

    create instances in gcp

---
+++ gcp/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("gcp.json")
+
+  project = "terraform-382820"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "app_example" {
+  count = 3
+  name = "terraform${count.index + 1}"
+  machine_type = "e2-micro"
+  zone = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      
+    }
+  }
+
+  tags = ["terraform${count.index + 1}"]
+
+  metadata = {
+    ssh-keys = "carlos:${file("../ssh/terraform.pub")}"
+  }
+}
