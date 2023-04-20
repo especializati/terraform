d5b48cf455ef61fc1e1e8d98f84972e71881299f
Carlos Ferreira <carlos@especializati.com.br>
2023-04-20 11:40:08 -0300

    feat: outputs

--- gcp/main.tf
+++ gcp/main.tf
@@ -101,3 +101,8 @@ resource "google_compute_firewall" "allow-http" {
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["http-server"]
 }
+
+output "instances_ips" {
+  # value = google_compute_instance.app_example[0].network_interface[0].access_config[0].nat_ip
+  value = [for instance in google_compute_instance.app_example : instance.network_interface[0].access_config[0].nat_ip]
+}
